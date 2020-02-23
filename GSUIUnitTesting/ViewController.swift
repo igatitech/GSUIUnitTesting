20a8c26b645498685dd7248a4452eb4ebe84b76e
igatsha <gati1993@gmail.com>
2020-02-23 21:52:52 +0530

    Initial Commit

---
+++ GSUIUnitTesting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GSUIUnitTesting
+//
+//  Created by Gati Shah on 23/02/20.
+//  Copyright © 2020 iGatiTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
